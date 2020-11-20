8cce493d277288d80efbd49ad0a66c66151489e1
lijiajia <lijiajia3515@163.com>
2020-11-20 14:11:09 +0800

    rebuild

--- distribution/build.sh
+++ distribution/build.sh
@@ -1,4 +1,4 @@
-docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-service:snapshot -f service/Dockerfile ./service
-docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-server:snapshot -f server/Dockerfile ./server
+docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-service:snapshot -f auth-service/Dockerfile ./auth-service
+docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-server:snapshot -f auth-server/Dockerfile ./auth-server
 docker push registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-service:snapshot
 docker push registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-server:snapshot
