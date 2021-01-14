ca47ed3f43b14584b76e512d63f7570c505bef0b
lijiajia <lijiajia3515@163.com>
2021-01-14 08:48:18 +0800

    这个故事还没有完结。——亚索

--- distribution/build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-service:snapshot -f auth-service/Dockerfile ./auth-service
-docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-server:snapshot -f auth-server/Dockerfile ./auth-server
-docker push registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-service:snapshot
-docker push registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-auth-server:snapshot
