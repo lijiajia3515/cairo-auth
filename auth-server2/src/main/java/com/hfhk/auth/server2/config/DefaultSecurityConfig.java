0fdf11f8a3df2d5bbdf2f865ae03888e09b73ad9
lijiajia <lijiajia3515@163.com>
2021-01-18 12:23:42 +0800

    这个故事还没有完结。——亚索

--- auth-server2/src/main/java/com/hfhk/auth/server2/config/DefaultSecurityConfig.java
+++ auth-server2/src/main/java/com/hfhk/auth/server2/config/DefaultSecurityConfig.java
@@ -2,10 +2,12 @@ package com.hfhk.auth.server2.config;
 
 import com.hfhk.auth.server2.modules.auth.HfhkAuthSuccessHandler;
 import com.hfhk.auth.server2.modules.auth.HfhkUserService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,23 +16,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class DefaultSecurityConfig {
 
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http, HfhkAuthSuccessHandler successHandler) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http,
+											@Qualifier("hfhkUserServices") UserDetailsService userDetailsService,
+											HfhkAuthSuccessHandler successHandler) throws Exception {
 		http
-			.csrf().disable()
+			//.csrf().disable()
 			.authorizeRequests(authorizeRequests -> authorizeRequests
 				.mvcMatchers("/actuator/**").permitAll()
 				.mvcMatchers("/test/**").permitAll()
+				.mvcMatchers("/login").permitAll()
 				.mvcMatchers("/**").authenticated()
 			)
 			.formLogin()
+			.loginPage("/login")
 			.successHandler(successHandler)
 			.and()
 			//.oauth2Login()
 			//.and()
 			//.oauth2Client()
 			//.and()
-			.sessionManagement()
-			.enableSessionUrlRewriting(true)
+			.rememberMe()
+			.userDetailsService(userDetailsService)
 			.and()
 			.exceptionHandling()
 			.and();
@@ -53,7 +59,7 @@ public class DefaultSecurityConfig {
 	}
 
 	@Bean
-	HfhkUserService users(MongoTemplate mongoTemplate) {
+	HfhkUserService hfhkUserServices(MongoTemplate mongoTemplate) {
 		return new HfhkUserService(mongoTemplate);
 	}
 
