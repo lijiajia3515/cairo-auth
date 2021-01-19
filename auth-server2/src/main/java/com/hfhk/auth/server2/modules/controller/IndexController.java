ddd988a595be2f95bd113ecfc0ff3250555f3f54
lijiajia <lijiajia3515@163.com>
2021-01-19 17:14:01 +0800

    这个故事还没有完结。——亚索

--- auth-server2/src/main/java/com/hfhk/auth/server2/modules/controller/IndexController.java
+++ auth-server2/src/main/java/com/hfhk/auth/server2/modules/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.hfhk.auth.server2.modules.controller;
 
 import com.hfhk.auth.server2.domain.AuthUser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class IndexController {
 	@GetMapping
-	public String index(@AuthenticationPrincipal AuthUser user, Model model) {
+	public String index(Authentication authentication, @AuthenticationPrincipal AuthUser user, Model model) {
+		log.info("[authentication]: {}", authentication);
+		model.addAttribute("authentication", authentication);
 		model.addAttribute("user", user);
 		return "index";
 	}
@@ -21,6 +24,11 @@ public class IndexController {
 	@GetMapping("/login")
 	public String login() {
 		return "login";
+	}
+
+	@GetMapping("/logout")
+	public String logout() {
+		return "logout";
 	}
 
 
