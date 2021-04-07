ccd298b590470ee1837bfcaf013f760cb8851d92
lijiajia <lijiajia3515@163.com>
2021-04-07 09:20:05 +0800

    这个故事还没有完结

--- cairo-auth-server/src/main/java/com/lijiajia3515/cairo/auth/server/constants/Redis.java
+++ cairo-auth-server/src/main/java/com/lijiajia3515/cairo/auth/server/constants/Redis.java
@@ -1,10 +1,10 @@
 package com.lijiajia3515.cairo.auth.server.constants;
 
 public class Redis {
-	public static final String KEY_PREFIX = "Hfhk:Service:Auth:";
+	public static final String KEY_PREFIX = "auth-server:";
 
 	public static class UserInfo {
-		private static final String KEY = KEY_PREFIX.concat("UserInfo:%s:%s");
+		private static final String KEY = KEY_PREFIX.concat("userinfo:%s:%s");
 
 		public static String key(String client, String uid) {
 			return String.format(KEY, client, uid);
